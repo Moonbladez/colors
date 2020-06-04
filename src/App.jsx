a24ff313eb0b73b37cd5ea31560e799ad2462d61
Bekki Young <moonbladez@hotmail.com>
2020-06-04 12:50:50 +0200

    generated color scale

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { Palette } from "./Palette/Palette";
+import { generatePalette } from "./ColorHelpers/colorHelpers";
 import SeedColors from "./SeedColors/SeedColors";
+import { render } from "@testing-library/react";
 
-function App() {
-	return (
-		<div className='App'>
-			<Palette palette={SeedColors[1]} />
-		</div>
-	);
+class App extends Component {
+	render() {
+		console.log(generatePalette(SeedColors[4]));
+		return (
+			<div className='App'>
+				<Palette palette={SeedColors[1]} />
+			</div>
+		);
+	}
 }
 
 export default App;
